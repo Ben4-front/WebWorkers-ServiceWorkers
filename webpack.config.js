bdab76653de211e210c7776f9db3155138615851
Ben4 <benymany511@gmail.com>
2026-02-11 19:31:52 +0300

    first commit

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
+      filename: 'index.html',
     }),
     new MiniCssExtractPlugin({
       filename: '[name].[contenthash].css',
